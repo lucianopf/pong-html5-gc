34599ff46e7626380d99b09bde4c0827627911aa
Lucianopf <lucianopf@outlook.com>
2016-06-15 17:30:51 -0300

    structuring

--- js/controls/keyboard.js
+++ js/controls/keyboard.js
@@ -1,5 +1,5 @@
-window.addEventListener('keyup', function(event) { Key.onKeyup(event); }, false);
-window.addEventListener('keydown', function(event) { Key.onKeydown(event); }, false);
+window.addEventListener('keyup', function (event) { Key.onKeyup(event); }, false)
+window.addEventListener('keydown', function (event) { Key.onKeydown(event); }, false)
 
 var Key = {
   _pressed: {},
@@ -9,16 +9,16 @@ var Key = {
   D: 68,
   S: 83,
   SPACE: 32,
-  
-  isDown: function(keyCode) {
-    return this._pressed[keyCode];
+
+  isDown: function (keyCode) {
+    return this._pressed[keyCode]
   },
-  
-  onKeydown: function(event) {
-    this._pressed[event.keyCode] = true;
+
+  onKeydown: function (event) {
+    this._pressed[event.keyCode] = true
   },
-  
-  onKeyup: function(event) {
-    delete this._pressed[event.keyCode];
+
+  onKeyup: function (event) {
+    delete this._pressed[event.keyCode]
   }
-};
+}
